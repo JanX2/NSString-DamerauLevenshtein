06dd22a9f5ac796d6413b885d193fd2eed4c0c1c
Jan <github@geheimwerk.de>
2011-01-02 21:10:51 +0100

    Adding testing harness and simple tests.

---
+++ DamerauLevenshteinTests.h
@@ -0,0 +1,16 @@
+//
+//  DamerauLevenshteinTests.h
+//  Damerau-Levenshtein
+//
+//  Created by Jan on 02.01.11.
+//  Copyright 2011 geheimwerk.de. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface DamerauLevenshteinTests : SenTestCase {
+
+}
+
+@end
---
+++ DamerauLevenshteinTests.m
@@ -0,0 +1,41 @@
+//
+//  DamerauLevenshteinTests.m
+//  Damerau-Levenshtein
+//
+//  Created by Jan on 02.01.11.
+//  Copyright 2011 geheimwerk.de. All rights reserved.
+//
+
+#import "DamerauLevenshteinTests.h"
+
+#import "NSString+DamerauLevenshtein.h"
+
+@implementation DamerauLevenshteinTests
+
+- (void)test_empty {
+	STAssertEquals((NSUInteger)0, [@"" distanceFromString:@""], @"Empty test #1 failed.");
+
+	STAssertEquals((NSUInteger)1, [@"" distanceFromString:@"a"], @"Empty test #2 failed.");
+
+	STAssertEquals((NSUInteger)1, [@"a" distanceFromString:@""], @"Empty test #3 failed.");
+}
+
+- (void)test_simple {
+	STAssertEquals((NSUInteger)1, [@"ab" distanceFromString:@"abc"], @"Simple insertion test failed.");
+
+	STAssertEquals((NSUInteger)1, [@"ab" distanceFromString:@"a"], @"Simple deletion test failed.");
+
+	STAssertEquals((NSUInteger)1, [@"ab" distanceFromString:@"az"], @"Simple substitution test failed.");
+
+#ifndef DISABLE_DAMERAU_TRANSPOSITION
+	STAssertEquals((NSUInteger)1, [@"ab" distanceFromString:@"ba"], @"Simple transposition test failed.");
+#endif
+}
+
+#ifndef DISABLE_DAMERAU_TRANSPOSITION
+- (void)test_restricted {
+	STAssertEquals((NSUInteger)3, [@"CA" distanceFromString:@"ABC"], @"Restricted test failed.");
+}
+#endif
+
+@end
