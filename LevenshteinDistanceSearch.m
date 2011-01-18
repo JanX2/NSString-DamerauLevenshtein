b808e771bc59804427602109c6a5e2bf481dd1b8
Jan <github@geheimwerk.de>
2011-01-18 03:58:47 +0100

    Removing archiving support from LevenshteinDistanceSearch. This makes the code much more readable.

--- LevenshteinDistanceSearch.m
+++ LevenshteinDistanceSearch.m
@@ -7,8 +7,6 @@
 #define MAX_COST	1
 
 // Apparently, recreating the trie from the raw word list is faster than archiving/unarchiving using NSKeyedArchiver. 
-#define ENABLE_ARCHIVING		0
-#define ENABLE_ARRAY_ARCHIVING	0
 
 int main (int argc, const char * argv[]) {
 	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
@@ -38,69 +36,22 @@ int main (int argc, const char * argv[]) {
 		dictionary = DICTIONARY;
 	}
 	
-#if ENABLE_ARCHIVING
-	NSString *archivePath = [NSTemporaryDirectory() stringByAppendingPathComponent:@"trie.archive"];
-#endif
-
-#if !ENABLE_ARCHIVING && ENABLE_ARRAY_ARCHIVING
-	NSString *arrayArchivePath = [NSTemporaryDirectory() stringByAppendingPathComponent:@"word-list.archive"];
-#endif
-	
+	// Read dictionary file into a trie
 	JXTrie *trie;
+	NSArray *wordList;
 	
-#if ENABLE_ARCHIVING
-	if ([[NSFileManager defaultManager] fileExistsAtPath:archivePath]) {
-		start = [NSDate timeIntervalSinceReferenceDate];
-		trie = [NSKeyedUnarchiver unarchiveObjectWithFile:archivePath];
-		duration = [NSDate timeIntervalSinceReferenceDate] - start;
-		NSLog(@"Read trie from archive: %lu words, %lu nodes. ", (unsigned long)[trie count], (unsigned long)[trie nodeCount]);
-		NSLog(@"Reading the trie took %.4lf s. ", (double)duration);
-	}
-	else {
-#endif
-		// Read dictionary file into a trie
-		NSArray *wordList;
-		
-		start = [NSDate timeIntervalSinceReferenceDate];
-#if !ENABLE_ARCHIVING && ENABLE_ARRAY_ARCHIVING
-		if ([[NSFileManager defaultManager] fileExistsAtPath:arrayArchivePath]) {
-			wordList = [NSKeyedUnarchiver unarchiveObjectWithFile:arrayArchivePath];
-		}
-		else {
-#endif
+	start = [NSDate timeIntervalSinceReferenceDate];
 
-			NSString *wordListText = [NSString stringWithContentsOfFile:dictionary encoding:NSUTF8StringEncoding error:NULL];
-			wordList = [wordListText componentsSeparatedByString:@"\n"];
-			
-#if !ENABLE_ARCHIVING && ENABLE_ARRAY_ARCHIVING
-			BOOL result = [NSKeyedArchiver archiveRootObject:wordList
-													  toFile:arrayArchivePath];
-			if (result) {
-				NSLog(@"Successfully archived word list to \"%@\". ", arrayArchivePath);
-			}
-			else {
-				NSLog(@"Archiving word list to \"%@\" failed! ", arrayArchivePath);
-			}
-		}
-#endif
+	NSString *wordListText = [NSString stringWithContentsOfFile:dictionary encoding:NSUTF8StringEncoding error:NULL];
+	wordList = [wordListText componentsSeparatedByString:@"\n"];
 		
-		trie = [JXTrie trieWithStrings:wordList];
-		duration = [NSDate timeIntervalSinceReferenceDate] - start;
+	trie = [JXTrie trieWithStrings:wordList];
+	
+	duration = [NSDate timeIntervalSinceReferenceDate] - start;
+	
+	NSLog(@"Read %lu words into %lu nodes. ", (unsigned long)[trie count], (unsigned long)[trie nodeCount]);
+	NSLog(@"Creating the trie for \"%@\" took %.4lf s. ", dictionary, (double)duration);
 		
-		NSLog(@"Read %lu words into %lu nodes. ", (unsigned long)[trie count], (unsigned long)[trie nodeCount]);
-		NSLog(@"Creating the trie for \"%@\" took %.4lf s. ", dictionary, (double)duration);
-		
-#if ENABLE_ARCHIVING
-		BOOL result = [NSKeyedArchiver archiveRootObject:trie
-												  toFile:archivePath];
-		if (result) {
-			NSLog(@"Successfully archived to \"%@\". ", archivePath);
-		}
-		else {
-			NSLog(@"Archiving to \"%@\" failed! ", archivePath);
-		}
-	}
-#endif
 	
 	NSArray *results = nil;
 	
