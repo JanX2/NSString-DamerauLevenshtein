f81edcd046ab092d976c01a5e4967953783a9ee7
Jan <github@geheimwerk.de>
2011-01-18 07:32:03 +0100

    Adding JXLDStringDistanceUtilities to JXTrie and removing code that is now redundant.

--- JXTrie/JXTrie.m
+++ JXTrie/JXTrie.m
@@ -9,19 +9,8 @@
 #import "JXTrie.h"
 
 #import "JXTrieNode.h"
+#import "JXLDStringDistanceUtilities.h"
 
-
-// Return the minimum of a, b and c - used by distanceFromString:options:
-CF_INLINE CFIndex smallestCFIndex(CFIndex a, CFIndex b, CFIndex c) {
-	CFIndex min = a;
-	if ( b < min )
-		min = b;
-	
-	if ( c < min )
-		min = c;
-	
-	return min;
-}
 
 @interface JXTrie ()
 @property (nonatomic, retain) JXTrieNode *rootNode;
