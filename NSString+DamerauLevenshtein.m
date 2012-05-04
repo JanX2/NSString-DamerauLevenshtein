65c94eaef9f258b6729909fe33bc94e13a8a8e6f
Jan <github@geheimwerk.de>
2012-05-04 11:28:51 +0200

    Refactoring core levenstein algorithm into levensteinUniCharDistanceCore().

--- NSString+DamerauLevenshtein.m
+++ NSString+DamerauLevenshtein.m
@@ -18,6 +18,7 @@
 @implementation NSString (DamerauLevenshtein)
 
 CFIndex levensteinStringDistance(CFStringRef string1, CFStringRef string2);
+CFIndex levensteinUniCharDistanceCore(const UniChar *string1_chars, CFIndex n, const UniChar *string2_chars, CFIndex m);
 
 - (NSUInteger)distanceFromString:(NSString *)comparisonString;
 {
@@ -57,9 +58,6 @@ CFIndex levensteinStringDistance(CFStringRef string1, CFStringRef string2);
 }
 
 CFIndex levensteinStringDistance(CFStringRef string1, CFStringRef string2) {
-#define string1CharacterAtIndex(A)	string1_chars[(A)]
-#define string2CharacterAtIndex(A)	string2_chars[(A)]
-	
 	// This implementation can be improved further if execution speed or memory constraints should ever pose a problem:
 	// http://en.wikipedia.org/wiki/Levenstein_Distance#Possible_improvements
 	
@@ -94,90 +92,101 @@ CFIndex levensteinStringDistance(CFStringRef string1, CFStringRef string2) {
 		const UniChar *string2_chars;
 		jxld_CFStringPrepareUniCharBuffer(string2, &string2_chars, &string2_buffer, CFRangeMake(0, m));
 		
-		// Step 1b
-		// Indexes into strings string1 and string2
-		CFIndex i;		// Iterates through string1
-		CFIndex j;		// Iterates through string2
-		
-		CFIndex cost;
-				
-		// Ignore common prefix (reducing memory footprint).
-		while (m > 0 && n > 0 && (*string1_chars == *string2_chars)) {
-			m -= 1;
-			n -= 1;
-			string1_chars++;
-			string2_chars++;
-		}
-		
-		// Ignore common suffix (reducing memory footprint).
-		while (m > 0 && n > 0 && (string1_chars[n-1] == string2_chars[m-1])) {
-			m -= 1;
-			n -= 1;
-		}
-		
-		// This implementation is based on Chas Emerick’s Java implementation:
-		// http://www.merriampark.com/ldjava.htm
-		
-		CFIndex *d = malloc((n+1) * sizeof(CFIndex));	// Cost array, horizontally
-		CFIndex *p = malloc((n+1) * sizeof(CFIndex));	// 'previous' cost array, horizontally
-		CFIndex *_d;			// Placeholder to assist in swapping p and d
-
-#ifndef DISABLE_DAMERAU_TRANSPOSITION
-		CFIndex *p2 = malloc((n+1) * sizeof(CFIndex));	// cost array before 'previous', horizontally
-#endif
-		
-		// Step 2
-		for (i = 0; i <= n; i++) {
-			p[i] = i;
-		}
-		
-		// Step 3 and 4
-		for (j = 1; j <= m; j++) {
-			d[0] = j;
-			
-			for (i = 1; i <= n; i++) {
-				// Step 5
-				cost = (string1CharacterAtIndex(i-1) == string2CharacterAtIndex(j-1)) ? 0 : 1;
-
-				// Step 6
-				// Minimum of cell to the left+1, to the top+1, diagonally left and up +cost				
-				d[i] = MIN(MIN(d[i-1]+1, p[i]+1),  p[i-1]+cost);  
-				
-#ifndef DISABLE_DAMERAU_TRANSPOSITION
-				// This conditional adds Damerau transposition to the Levenshtein distance
-				if (i > 1 && j > 1 
-					&& string1CharacterAtIndex(i-1) == string2CharacterAtIndex(j-2) 
-					&& string1CharacterAtIndex(i-2) == string2CharacterAtIndex(j-1) )
-				{
-					d[i] = MIN(d[i], p2[i-2] + cost );
-				}
-#endif
-			}
-			
-			// Copy current distance counts to 'previous row' distance counts
-#ifdef DISABLE_DAMERAU_TRANSPOSITION
-			_d = p;
-#else
-			_d = p2;
-			p2 = p;
-#endif
-			p = d;
-			d = _d;
-		} 
-		
-		// Our last action in the above loop was to switch d and p, so p now 
-		// actually has the most recent cost counts
-		distance = p[n];
-		
-		free(d);
-		free(p);
-#ifndef DISABLE_DAMERAU_TRANSPOSITION
-		free(p2);
-#endif
+		distance = levensteinUniCharDistanceCore(string1_chars, n, string2_chars, m);
 	}
 	
 	if (string1_buffer != NULL) free(string1_buffer);
 	if (string2_buffer != NULL) free(string2_buffer);
+	
+	return distance;
+}
+
+CFIndex levensteinUniCharDistanceCore(const UniChar *string1_chars, CFIndex n, const UniChar *string2_chars, CFIndex m) {
+#define string1CharacterAtIndex(A)	string1_chars[(A)]
+#define string2CharacterAtIndex(A)	string2_chars[(A)]
+	
+	CFIndex distance = 0;
+	
+	// Step 1b
+	// Indexes into strings string1 and string2
+	CFIndex i;		// Iterates through string1
+	CFIndex j;		// Iterates through string2
+	
+	CFIndex cost;
+	
+	// Ignore common prefix (reducing memory footprint).
+	while (m > 0 && n > 0 && (*string1_chars == *string2_chars)) {
+		m -= 1;
+		n -= 1;
+		string1_chars++;
+		string2_chars++;
+	}
+	
+	// Ignore common suffix (reducing memory footprint).
+	while (m > 0 && n > 0 && (string1_chars[n-1] == string2_chars[m-1])) {
+		m -= 1;
+		n -= 1;
+	}
+	
+	// This implementation is based on Chas Emerick’s Java implementation:
+	// http://www.merriampark.com/ldjava.htm
+	
+	CFIndex *d = malloc((n+1) * sizeof(CFIndex));	// Cost array, horizontally
+	CFIndex *p = malloc((n+1) * sizeof(CFIndex));	// 'previous' cost array, horizontally
+	CFIndex *_d;			// Placeholder to assist in swapping p and d
+	
+#ifndef DISABLE_DAMERAU_TRANSPOSITION
+	CFIndex *p2 = malloc((n+1) * sizeof(CFIndex));	// cost array before 'previous', horizontally
+#endif
+	
+	// Step 2
+	for (i = 0; i <= n; i++) {
+		p[i] = i;
+	}
+	
+	// Step 3 and 4
+	for (j = 1; j <= m; j++) {
+		d[0] = j;
+		
+		for (i = 1; i <= n; i++) {
+			// Step 5
+			cost = (string1CharacterAtIndex(i-1) == string2CharacterAtIndex(j-1)) ? 0 : 1;
+			
+			// Step 6
+			// Minimum of cell to the left+1, to the top+1, diagonally left and up +cost				
+			d[i] = MIN(MIN(d[i-1]+1, p[i]+1),  p[i-1]+cost);  
+			
+#ifndef DISABLE_DAMERAU_TRANSPOSITION
+			// This conditional adds Damerau transposition to the Levenshtein distance
+			if (i > 1 && j > 1 
+				&& string1CharacterAtIndex(i-1) == string2CharacterAtIndex(j-2) 
+				&& string1CharacterAtIndex(i-2) == string2CharacterAtIndex(j-1) )
+			{
+				d[i] = MIN(d[i], p2[i-2] + cost );
+			}
+#endif
+		}
+		
+		// Copy current distance counts to 'previous row' distance counts
+#ifdef DISABLE_DAMERAU_TRANSPOSITION
+		_d = p;
+#else
+		_d = p2;
+		p2 = p;
+#endif
+		p = d;
+		d = _d;
+	} 
+	
+	// Our last action in the above loop was to switch d and p, so p now 
+	// actually has the most recent cost counts
+	distance = p[n];
+	
+	free(d);
+	free(p);
+#ifndef DISABLE_DAMERAU_TRANSPOSITION
+	free(p2);
+#endif
 	
 	return distance;
 	
