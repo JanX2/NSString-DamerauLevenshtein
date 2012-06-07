c1d0b3b68f5f01820068349b5f1a6b471a37aa08
Jan <github@geheimwerk.de>
2012-06-07 21:49:55 +0200

    Improving comments.

--- JXLDStringDistance.h
+++ JXLDStringDistance.h
@@ -11,10 +11,10 @@ enum {
     JXLDCaseInsensitiveComparison = 1,			/* If specified, ignores the case (a == A) */
 	JXLDLiteralComparison = 2,					/* Exact character-by-character equivalence */
 	JXLDWhitespaceInsensitiveComparison = 4,	/* If specified, ignores white space */
-	JXLDWhitespaceTrimmingComparison = 8,		/* If specified, trims white space from both ends (" A " == "A") */
+	JXLDWhitespaceTrimmingComparison = 8,		/* If specified, trims white space from both ends (" A B C " == "A B C") */
     JXLDDiacriticInsensitiveComparison = 128,	/* If specified, ignores diacritics (o-umlaut == o) */
     JXLDWidthInsensitiveComparison = 256,		/* If specified, ignores width differences ('a' == U+FF41) */
-    JXLDDelimiterInsensitiveComparison = 512,	/* If specified, replaces common delimiters ('_', '-') with space (' ') */
+    JXLDDelimiterInsensitiveComparison = 512,	/* If specified, replaces common delimiters ('_', '-') with space (' ') before comparison */
 };
 typedef NSUInteger JXLDStringDistanceOptions;
 
@@ -22,5 +22,6 @@ typedef NSUInteger JXLDStringDistanceOptions;
 Important:
  
 Please note that JXLDDelimiterInsensitiveComparison is processed before JXLDWhitespaceInsensitiveComparison and JXLDWhitespaceTrimmingComparison.
-This results in the affected delimiter characters being removed for the comparison (all from the former option, only those at both ends for the latter).
+This results is that the affected delimiter characters are effectively being removed before the comparison takes place
+(all for the former option, only those at both ends for the latter).
 */
