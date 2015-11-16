52abebc71cea60a8a4743510a388244a21df01b9
Jan <github@geheimwerk.de>
2015-11-16 22:10:56 +0100

    Reindenting.

--- Damerau-Levenshtein.m
+++ Damerau-Levenshtein.m
@@ -3,48 +3,48 @@
 #import "NSString+DamerauLevenshtein.h"
 
 int main (int argc, const char * argv[]) {
-
+	
 	@autoreleasepool {
-
-	NSString *string1 = @"Hello, World!";
-	NSString *string2 = @"Hallo, Welt!";
-	NSString *string1trans = @"Hello, Wrold!";
-	
-	NSUInteger levensteinDistance;
-	
-	levensteinDistance = [string1 distanceFromString:string2];
-	NSLog(@"\nThe Levenstein distance between\n\"%@\"\nand\n\"%@\"\n=\n%ld\n\n", string1, string2, (long)levensteinDistance);
-	
-	levensteinDistance = [string1 distanceFromString:string1trans];
-	NSString *resultDescription;
-	switch (levensteinDistance) {
-		case 1:
-			resultDescription = @"enabled";
-			break;
-		case 2:
-			resultDescription = @"disabled";
-			break;
-		default:
-			resultDescription = @"broken";
-			break;
-	}
-
-	NSLog(@"Damerau transposition %@. \n\n", resultDescription);
-
-	float similarity = [string1 similarityToString:string2];
-	NSLog(@"\nThe similarity between\n\"%@\"\nand\n\"%@\"\n=\n%f\n\n", string1, string2, similarity);
-	
-	JXLDWeights weights = (JXLDWeights){
-		.phrase_to_word = 0.75f,
-		.min = 0.8f,
-		.max = 0.1f,
-		.length = 0.1f,
-	};
-	weights = JXLDWeightsNormalize(weights);
-	
-	float semanticSimilarity = [string1 semanticSimilarityToString:string2 weights:weights];
-	NSLog(@"\nThe semantic similarity between\n\"%@\"\nand\n\"%@\"\n=\n%f\n\n", string1, string2, semanticSimilarity);
-
+		
+		NSString *string1 = @"Hello, World!";
+		NSString *string2 = @"Hallo, Welt!";
+		NSString *string1trans = @"Hello, Wrold!";
+		
+		NSUInteger levensteinDistance;
+		
+		levensteinDistance = [string1 distanceFromString:string2];
+		NSLog(@"\nThe Levenstein distance between\n\"%@\"\nand\n\"%@\"\n=\n%ld\n\n", string1, string2, (long)levensteinDistance);
+		
+		levensteinDistance = [string1 distanceFromString:string1trans];
+		NSString *resultDescription;
+		switch (levensteinDistance) {
+			case 1:
+				resultDescription = @"enabled";
+				break;
+			case 2:
+				resultDescription = @"disabled";
+				break;
+			default:
+				resultDescription = @"broken";
+				break;
+		}
+		
+		NSLog(@"Damerau transposition %@. \n\n", resultDescription);
+		
+		float similarity = [string1 similarityToString:string2];
+		NSLog(@"\nThe similarity between\n\"%@\"\nand\n\"%@\"\n=\n%f\n\n", string1, string2, similarity);
+		
+		JXLDWeights weights = (JXLDWeights){
+			.phrase_to_word = 0.75f,
+			.min = 0.8f,
+			.max = 0.1f,
+			.length = 0.1f,
+		};
+		weights = JXLDWeightsNormalize(weights);
+		
+		float semanticSimilarity = [string1 semanticSimilarityToString:string2 weights:weights];
+		NSLog(@"\nThe semantic similarity between\n\"%@\"\nand\n\"%@\"\n=\n%f\n\n", string1, string2, semanticSimilarity);
+		
 	}
 	
 	return 0;
--- LevenshteinDistanceSearch.m
+++ LevenshteinDistanceSearch.m
@@ -6,63 +6,63 @@
 #define TARGET	@"goober"
 #define MAX_COST	1
 
-// Apparently, recreating the trie from the raw word list is faster than archiving/unarchiving using NSKeyedArchiver. 
+// Apparently, recreating the trie from the raw word list is faster than archiving/unarchiving using NSKeyedArchiver.
 
 int main (int argc, const char * argv[]) {
-
+	
 	@autoreleasepool {
-
-	NSString *target;
-	NSUInteger maxCost;
-	NSString *dictionary;
-	
-	NSTimeInterval start;
-	NSTimeInterval duration;
-	
-	if ([NSProcessInfo processInfo].arguments.count < 3) {
-		fprintf(stderr, "usage: %s [<search string> <maximum distance>] [<dictionary path>]\n",
-				[NSProcessInfo processInfo].processName.UTF8String);
-		target = TARGET;
-		maxCost = MAX_COST;
-	}
-	else {
-		target = [NSProcessInfo processInfo].arguments[1];
-		maxCost = [NSProcessInfo processInfo].arguments[2].integerValue;
-	}
-
-	if ([NSProcessInfo processInfo].arguments.count > 3) {
-		dictionary = [NSProcessInfo processInfo].arguments[3];
-	}
-	else {
-		dictionary = DICTIONARY;
-	}
-	
-	// Read dictionary file into a trie
-	JXTrie *trie;
-	
-	start = [NSDate timeIntervalSinceReferenceDate];
-
-	NSString *wordListText = [NSString stringWithContentsOfFile:dictionary encoding:NSUTF8StringEncoding error:NULL];
 		
-	trie = [JXTrie trieWithWordListString:wordListText];
-	
-	duration = [NSDate timeIntervalSinceReferenceDate] - start;
-	
-	//NSLog(@"\n\n%@", trie);
-	NSLog(@"Read %lu words into %lu nodes. ", (unsigned long)trie.count, (unsigned long)trie.nodeCount);
-	NSLog(@"Creating the trie for \"%@\" took %.4f s. ", dictionary, (double)duration);
+		NSString *target;
+		NSUInteger maxCost;
+		NSString *dictionary;
 		
-	
-	NSArray *results = nil;
-	
-	start = [NSDate timeIntervalSinceReferenceDate];
-	results = [trie search:target maximumDistance:maxCost];
-	duration = [NSDate timeIntervalSinceReferenceDate] - start;
-	
-	NSLog(@"\n%@", results);
-	
-	NSLog(@"Search for \"%@\" took %.4f s. ", target, (double)duration);
-
+		NSTimeInterval start;
+		NSTimeInterval duration;
+		
+		if ([NSProcessInfo processInfo].arguments.count < 3) {
+			fprintf(stderr, "usage: %s [<search string> <maximum distance>] [<dictionary path>]\n",
+					[NSProcessInfo processInfo].processName.UTF8String);
+			target = TARGET;
+			maxCost = MAX_COST;
+		}
+		else {
+			target = [NSProcessInfo processInfo].arguments[1];
+			maxCost = [NSProcessInfo processInfo].arguments[2].integerValue;
+		}
+		
+		if ([NSProcessInfo processInfo].arguments.count > 3) {
+			dictionary = [NSProcessInfo processInfo].arguments[3];
+		}
+		else {
+			dictionary = DICTIONARY;
+		}
+		
+		// Read dictionary file into a trie
+		JXTrie *trie;
+		
+		start = [NSDate timeIntervalSinceReferenceDate];
+		
+		NSString *wordListText = [NSString stringWithContentsOfFile:dictionary encoding:NSUTF8StringEncoding error:NULL];
+		
+		trie = [JXTrie trieWithWordListString:wordListText];
+		
+		duration = [NSDate timeIntervalSinceReferenceDate] - start;
+		
+		//NSLog(@"\n\n%@", trie);
+		NSLog(@"Read %lu words into %lu nodes. ", (unsigned long)trie.count, (unsigned long)trie.nodeCount);
+		NSLog(@"Creating the trie for \"%@\" took %.4f s. ", dictionary, (double)duration);
+		
+		
+		NSArray *results = nil;
+		
+		start = [NSDate timeIntervalSinceReferenceDate];
+		results = [trie search:target maximumDistance:maxCost];
+		duration = [NSDate timeIntervalSinceReferenceDate] - start;
+		
+		NSLog(@"\n%@", results);
+		
+		NSLog(@"Search for \"%@\" took %.4f s. ", target, (double)duration);
+		
 	}
 	
 	return 0;
