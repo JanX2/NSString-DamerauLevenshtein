656325f357ea7a152b712e672759cded3671b271
Jan <github@geheimwerk.de>
2015-12-01 11:43:42 +0100

    Rewriting CFCharacterSet-based functions using forward search.

--- JXLDStringDistanceUtilities.m
+++ JXLDStringDistanceUtilities.m
@@ -8,22 +8,80 @@
 
 #import "JXLDStringDistanceUtilities.h"
 
-void jxld_CFStringRemoveWhitespace(CFMutableStringRef string) {
+#define USE_BACKWARD_SEARCH	0 // Backward search has been used for a long time, because of its simpler implementation.
+
+CF_INLINE void updateSearchRangeAfterModification(CFRange * const search_range_p, const CFRange result_range, const CFIndex replacement_length) {
+	const CFIndex old_location = search_range_p->location;
+	const CFIndex new_location = result_range.location + replacement_length;
+	const CFIndex location_change = old_location - new_location; // usually negative
+	const CFIndex length_change = replacement_length - result_range.length; // usually negative
+	search_range_p->location = new_location;
+	search_range_p->length += location_change + length_change;
+}
+
+CF_INLINE void jxld_CFStringDeleteCharactersFromSet(CFMutableStringRef string, CFCharacterSetRef characterSet) {
 	CFIndex string_length = CFStringGetLength(string);
 	
+	CFRange result_range;
+	CFRange search_range = CFRangeMake(0, string_length);
+	
+#if USE_BACKWARD_SEARCH
+	while ( CFStringFindCharacterFromSet(string,
+										 characterSet,
+										 search_range,
+										 kCFCompareBackwards,
+										 &result_range) ) {
+		CFStringDelete(string, result_range);
+		search_range.length = result_range.location; // We can do this safely, because we use kCFCompareBackwards
+	}
+#else
+	const CFIndex replacement_length = 0;
+	
+	while ( CFStringFindCharacterFromSet(string,
+										 characterSet,
+										 search_range,
+										 0,
+										 &result_range) ) {
+		CFStringDelete(string, result_range);
+		updateSearchRangeAfterModification(&search_range, result_range, replacement_length);
+	}
+#endif
+}
+
+CF_INLINE void jxld_CFStringReplaceCharactersFromSet(CFMutableStringRef string, CFCharacterSetRef characterSet, CFStringRef replacement) {
+	// We may be able to optimize this function by directly manipulating a UniChar buffer.
+	CFIndex string_length = CFStringGetLength(string);
+	
+	CFRange result_range;
+	CFRange search_range = CFRangeMake(0, string_length);
+	
+#if USE_BACKWARD_SEARCH
+	while ( CFStringFindCharacterFromSet(string,
+										 characterSet,
+										 search_range,
+										 kCFCompareBackwards,
+										 &result_range) ) {
+		CFStringReplace(string, result_range, replacement);
+		search_range.length = result_range.location; // We can do this safely, because we use kCFCompareBackwards
+	}
+#else
+	CFIndex replacement_length = CFStringGetLength(replacement);
+	
+	while ( CFStringFindCharacterFromSet(string,
+										 characterSet,
+										 search_range,
+										 0,
+										 &result_range) ) {
+		CFStringReplace(string, result_range, replacement);
+		updateSearchRangeAfterModification(&search_range, result_range, replacement_length);
+	}
+#endif
+}
+
+void jxld_CFStringRemoveWhitespace(CFMutableStringRef string) {
 	CFCharacterSetRef characterSetWhitespaceAndNewline = CFCharacterSetGetPredefined(kCFCharacterSetWhitespaceAndNewline);
 	
-	CFRange result;
-	CFRange range_to_search = CFRangeMake(0, string_length);
-	
-	while ( CFStringFindCharacterFromSet(string, 
-										 characterSetWhitespaceAndNewline, 
-										 range_to_search, 
-										 kCFCompareBackwards, 
-										 &result) ) {
-		CFStringDelete(string, result);
-		range_to_search.length = result.location; // We can do this safely, because we use kCFCompareBackwards
-	}
+	jxld_CFStringDeleteCharactersFromSet(string, characterSetWhitespaceAndNewline);
 }
 
 void jxld_CFStringReplaceDelimitersWithSpace(CFMutableStringRef string) {
@@ -58,20 +116,7 @@ void jxld_CFStringStraightenQuotes(CFMutableStringRef string) {
 }
 
 void jxld_CFStringReplaceCharactersInSet(CFMutableStringRef string, CFCharacterSetRef delimitersCharacterSet, CFStringRef replacement) {
-	// We may be able to optimize this function by directly manipulating a UniChar buffer.
-    CFIndex string_length = CFStringGetLength(string);
-	
-	CFRange result;
-	CFRange range_to_search = CFRangeMake(0, string_length);
-	
-	while ( CFStringFindCharacterFromSet(string, 
-										 delimitersCharacterSet, 
-										 range_to_search, 
-										 kCFCompareBackwards, 
-										 &result) ) {
-		CFStringReplace(string, result, replacement);
-		range_to_search.length = result.location; // We can do this safely, because we use kCFCompareBackwards
-	}
+	jxld_CFStringReplaceCharactersFromSet(string, delimitersCharacterSet, replacement);
 }
 
 void jxld_CFStringPreprocessWithOptions(CFMutableStringRef string, JXLDStringDistanceOptions options) {
